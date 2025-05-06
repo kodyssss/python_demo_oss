229cb615eb16d32e1b313f30c4a1f01940c46134
ybr_mac_os <9741771019@qq.com>
2025-05-06 16:46:45 +0800

    sec commit_ ososs

--- app/config.py
+++ app/config.py
@@ -4,9 +4,9 @@ class Config:
     STATIC_DIR = os.path.join(os.path.dirname(__file__), '../static')
     VERSION = "v1.0.0"
     # MySQL 配置
-    MYSQL_HOST = os.getenv('MYSQL_HOST', '127.0.0.1')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'xxxxx')
     MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'rancher')
     MYSQL_DB = os.getenv('MYSQL_DB', 'suse_db')
     # OSS 配置
     OSS_ACCESS_KEY_ID = os.getenv('OSS_ACCESS_KEY_ID', 'xxxxxx')
